01ab246fe835dcd0f21f78c3bc85f0001944803c
MaxFeldman1 <mackx@protonmail.com>
2020-09-22 22:20:55 -0500

    redeployed contracts as last deployment had faulty parameters

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -26,7 +26,7 @@ const kovanUniswapFactoryAddress = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
 const kovanUniswapRouterV2Address = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
 
 const kovanBigMathAddress = "0x54Ee4Eb03a7697600F1100814A949304887558B7";
-const kovanOrganizerAddress = "0x4605274d1E022038c5E749168Ce4B7EA7E59a26B";
+const kovanOrganizerAddress = "0xac99f768eb53Bd32Bc4d830a64DaFEDc876A5f4d";
 
 
 inflator0 = 4;
@@ -37,9 +37,9 @@ startTimestamp = "1593561600";
 lengthOfPriceSeries = "184";
 
 
-lastStakeTimestamp = "1600560000";
-endStakingTimestamp = "1600646400";
-destructionTimestamp = "1600732800";
+lastStakeTimestamp = "1609459200";
+endStakingTimestamp = "1609977600";
+destructionTimestamp = "1610582400";
 
 module.exports = async function(deployer) {
 
@@ -47,16 +47,33 @@ module.exports = async function(deployer) {
   oracleInstance = await oracle.at(kovanEthUsdOracleAddress);
   factoryInstance = await factory.at(kovanUniswapFactoryAddress);
   routerInstance = await router.at(kovanUniswapRouterV2Address);
-
   organizerInstance = await organizer.at(kovanOrganizerAddress);
-  tokenSubUnits = (new BN(10)).pow(new BN(18));
-  payoutAtVarianceOf1 = tokenSubUnits.div(new BN(5)).toString();
-  cap = tokenSubUnits.toString();
-  await organizerInstance.deployVarianceInstance("ETH/USD", tokenInstance.address, oracleInstance.address, startTimestamp,
-    lengthOfPriceSeries, payoutAtVarianceOf1, cap);
+/*
+  organizerInstance = await deployer.deploy(organizer, kovanBigMathAddress);
 
+  subUnits = (new BN(10)).pow(new BN(18));
+  payoutAtVarianceOf1 = subUnits.div(new BN(5)).toString();
+  cap = subUnits.toString();
+  await organizerInstance.deployVarianceInstance("ETH/USD", tokenInstance.address, kovanEthUsdOracleAddress,
+  	startTimestamp, lengthOfPriceSeries, payoutAtVarianceOf1, cap);
+  console.log('deployed variance contract');
   varianceSwapHandlerInstance = await varianceSwapHandler.at(await organizerInstance.varianceSwapInstances(0));
   longVarianceTokenInstance = await longVarianceToken.at(await varianceSwapHandlerInstance.longVarianceTokenAddress());
   shortVarianceTokenInstance = await shortVarianceToken.at(await varianceSwapHandlerInstance.shortVarianceTokenAddress());
+  console.log('got instances');
+  pair0 = await factoryInstance.createPair(tokenInstance.address, longVarianceTokenInstance.address);
+  pair0 = pair0.receipt.logs[0].args.pair;
+  pair1 = await factoryInstance.createPair(shortVarianceTokenInstance.address, longVarianceTokenInstance.address);
+  pair1 = pair1.receipt.logs[0].args.pair;
+  pair2 = await factoryInstance.createPair(shortVarianceTokenInstance.address, tokenInstance.address);
+  pair2 = pair2.receipt.logs[0].args.pair;
+  console.log('deployed uni pairs');
 
+  stakeHubInstance = await deployer.deploy(stakeHub, tokenInstance.address, pair0, pair1, pair2,
+  	inflator0, inflator1, inflator2, lastStakeTimestamp, endStakingTimestamp, destructionTimestamp);
+  await organizerInstance.addStakeHub(0, stakeHubInstance.address)
+  console.log('added stake hub');
+  await varianceSwapHandlerInstance.setFee('50');
+  console.log('fee has been set');
+*/
 }
