eacd54f960500a9c988e01897e3a649069a1059f
MaxFeldman1 <mackx@protonmail.com>
2021-01-24 02:41:33 -0600

    deploy to kovan

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,3 @@
-const token = artifacts.require("Token");
 const AToken = artifacts.require("DummyAToken");
 const bigMath = artifacts.require("BigMath");
 const factory = artifacts.require("UniswapV2Factory");
@@ -14,6 +13,7 @@ const oracleContainer = artifacts.require("OracleContainer");
 const baseAggregator = artifacts.require("dummyAggregator");
 const aggregatorFacade = artifacts.require("dummyAggregatorFacade");
 const lendingPool = artifacts.require("DummyLendingPool");
+const ILendingPool = artifacts.require("ILendingPool");
 
 const defaultAddress = "0x0000000000000000000000000000000000000000";
 const BN = web3.utils.BN;
@@ -22,56 +22,82 @@ const _feb12021 = "1612137600";
 
 const kovanETHUSDAggregatorFacadeAddr = "0x9326BFA02ADD2366b30bacB125260Af641031331";
 
+const kovanUniswapFactoryAddr = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
+const kovanUniswapRouter02Addr = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+
+const kovanAaveLendingPoolAddr = "0x9FE532197ad76c5a68961439604C037EB79681F0";
+const kovanAaveAUSDCAddr = "0xe12AFeC5aa12Cf614678f9bFeeB98cA9Bb95b5B0";
+
+const kovanOracleContainerAddr = "0xA43617A5d4Ef97fF9D989e6788ca31304C54Cb1D";
+const kovanDeployERC20TokensAddr = "0x4B5f6dBE5f610B286AdA2a59b044b92944Cc00B7";
+const kovanDeployStakeHubAddr = "0x311b328542EA500F56993589A7E257c32a15dc90";
+const kovanBigMathAddr = "0xA83D65bcc94762Eeee2a1433Da518bc6340ee1B4";
+const kovanOrganizerAddr = "0x163Ea98618570Ed4ca50C3427e322300c2706e52";
+
+
 module.exports = async function(deployer) {
+  tokenInstance = await AToken.at(kovanAaveAUSDCAddr);
 
-  underlyingAssetInstance = await deployer.deploy(token);
-  underlyingAssetInstance = await deployer.deploy(token);
+  lendingPoolInstance = await ILendingPool.at(kovanAaveLendingPoolAddr);
 
-  tokenInstance = await deployer.deploy(AToken, underlyingAssetInstance.address);
+  factoryInstance = await factory.at(kovanUniswapFactoryAddr);
+  routerInstance = await router.at(kovanUniswapRouter02Addr);
 
-  lendingPoolInstance = await deployer.deploy(lendingPool);
+  phrase = "ETH / USD";
 
-  factoryInstance = await deployer.deploy(factory, defaultAddress);
-  routerInstance = await deployer.deploy(router, factoryInstance.address,
-    /*This param does not impact functionality of this project*/defaultAddress);
+  aggregatorFacadeInstance = await aggregatorFacade.at(kovanETHUSDAggregatorFacadeAddr);
 
-  phrase = "ETH/USD";
+  fetchedPhrase = await aggregatorFacadeInstance.description();
 
-  baseAggregatorInstance = await baseAggregator.new(3);
-  aggregatorFacadeInstance = await aggregatorFacade.new(baseAggregatorInstance.address, phrase);
-  oracleContainerInstance = await oracleContainer.new();
+  if (phrase !== fetchedPhrase) {
+    console.log(phrase, fetchedPhrase, "OOF");
+    process.exit();
+  }
+
+  oracleContainerInstance = await deployer.deploy(oracleContainer);
+  //oracleContainerInstance = await oracleContainer.at(kovanOracleContainerAddr);
 
   await oracleContainerInstance.addAggregators([aggregatorFacadeInstance.address]);
+
   await oracleContainerInstance.deploy(phrase);
 
   tokenDeployerInstance = await deployer.deploy(deployERC20Tokens);
+  //tokenDeployerInstance = await deployERC20Tokens.at(kovanDeployERC20TokensAddr);
   stakeHubDeployerInstance = await deployer.deploy(deployStakeHub);
-
-  asset1 = await deployer.deploy(token);
-  asset2 = await deployer.deploy(token);
+  //stakeHubDeployerInstance = await deployStakeHub.at(kovanDeployStakeHubAddr);
 
   bigMathInstance = await deployer.deploy(bigMath);
+  //bigMathInstance = await bigMath.at(kovanBigMathAddr);
   payoutAtVariance1 = (new BN(10)).pow(await tokenInstance.decimals()).toString();
   cap = payoutAtVariance1.substring(0, payoutAtVariance1.length-1);
 
   organizerInstance = await deployer.deploy(organizer, bigMathInstance.address, oracleContainerInstance.address,
     tokenDeployerInstance.address, stakeHubDeployerInstance.address, lendingPoolInstance.address);
+  //organizerInstance = await organizer.at(kovanOrganizerAddr);
   await organizerInstance.deployVarianceInstance(phrase, tokenInstance.address,
     _feb12021, "30", payoutAtVariance1, cap);
+
   varianceSwapHandlerInstance = await varianceSwapHandler.at(await organizerInstance.varianceSwapInstances(0));
-  longVarianceTokenInstance = await longVarianceToken.at(await varianceSwapHandlerInstance.longVarianceTokenAddress());
-  shortVarianceTokenInstance = await shortVarianceToken.at(await varianceSwapHandlerInstance.shortVarianceTokenAddress());
-  pair0 = await factoryInstance.createPair(tokenInstance.address, longVarianceTokenInstance.address);
+
+
+  lvtAddress = await varianceSwapHandlerInstance.longVarianceTokenAddress();
+  svtAddress = await varianceSwapHandlerInstance.shortVarianceTokenAddress()
+
+  pair2 = await factoryInstance.getPair(svtAddress, tokenInstance.address);
+  stakeHubAddress = await organizerInstance.varianceToStakeHub(varianceSwapHandlerInstance.address);
+
+  pair0 = await factoryInstance.createPair(tokenInstance.address, lvtAddress);
   pair0 = pair0.receipt.logs[0].args.pair;
-  pair1 = await factoryInstance.createPair(shortVarianceTokenInstance.address, longVarianceTokenInstance.address);
+
+  pair1 = await factoryInstance.createPair(svtAddress, lvtAddress);
   pair1 = pair1.receipt.logs[0].args.pair;
-  pair2 = await factoryInstance.createPair(shortVarianceTokenInstance.address, tokenInstance.address);
+
+  pair2 = await factoryInstance.createPair(svtAddress, tokenInstance.address);
   pair2 = pair2.receipt.logs[0].args.pair;
+
   inflator0 = 4;
   inflator1 = 3;
   inflator2 = 2;
-  lastStakeTimestamp = 3000000000;
-  endStakingTimestamp = 3000010000;
-  destructionTimestamp = 3000020000;
   await organizerInstance.addStakeHub(0, pair0, pair1, pair2, inflator0, inflator1, inflator2);
+  //*/
 }
--- truffle-config.js
+++ truffle-config.js
@@ -18,9 +18,8 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const pk = "2a25f518d61b3676ca3feeec04e6f91dd274a8a3cede7b73924894a301f3268c";//
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
@@ -46,7 +45,7 @@ module.exports = {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
-    }//,
+    },
 
     // Another network with more advanced options...
     // advanced: {
@@ -60,14 +59,14 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+    kovan: {
+      provider: () => new HDWalletProvider(pk, `https://kovan.infura.io/v3/130607aa3e804a5a9feab69f92045243`),
+      network_id: 42,       // Ropsten's id
+      gas: 12487794,       // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    }//,
 
     // Useful for private networks
     // private: {
